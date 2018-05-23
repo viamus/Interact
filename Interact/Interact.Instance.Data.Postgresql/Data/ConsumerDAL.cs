9626ffe506bd306cb74e2cf5ddeb345dfeca78f1
Lucas F Valle <payvalle@gmail.com>
2018-05-22 22:39:08 -0300

    Implementation of te SetConsumerThreadStatus.

--- Interact/Interact.Instance.Data.Postgresql/Data/ConsumerDAL.cs
+++ Interact/Interact.Instance.Data.Postgresql/Data/ConsumerDAL.cs
@@ -5,8 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
-using System.Data;
 using System.Transactions;
+using Interact.Instance.Data.Exceptions;
 
 namespace Interact.Instance.Data.Postgresql.Data
 {
@@ -17,6 +17,12 @@ namespace Interact.Instance.Data.Postgresql.Data
             this.Dispose();
         }
 
+        public TransactionScope CreateTransactionScope(IsolationLevel isolation = IsolationLevel.ReadCommitted, TimeSpan? timeout = null)
+        {
+            timeout = timeout ?? new TimeSpan(0, 0, 30);
+            return new TransactionScope(TransactionScopeOption.Required, new TransactionOptions { IsolationLevel = IsolationLevel.ReadCommitted, Timeout = timeout.Value });
+        }
+
         public Library.Structure.ConsumerStatus GetThreadGroupServerStatus(string threadGroup)
         {
             using (var context = new InteractContext())
@@ -25,12 +31,51 @@ namespace Interact.Instance.Data.Postgresql.Data
             }
         }
 
-        public void SetConsumerThreadStatus(string threadGroup, string indentifier, Library.Structure.ConsumerStatus clientStatus)
+        public void SetConsumerThreadStatus(string threadGroup, string identifier, Library.Structure.ConsumerStatus clientStatus)
         {
             using (var context = new InteractContext())
             {
+                using (var scope = CreateTransactionScope())
+                {
+                    var clientInstance = context.CloudClientInstance.Where(c => c.Identifier == identifier).FirstOrDefault();
 
+                    if(clientInstance == null)
+                    {
+                        var serverInstance = GetCloudInstance(threadGroup);
 
+                        if (serverInstance == null)
+                        {
+                            throw new NotFoundException($"Consumer Instance not fount to the threadgroup = {threadGroup}");
+                        }
+
+                        context.CloudClientInstance.Add(new CloudClientInstance
+                        {
+                            Identifier = identifier,
+                            CloudInstanceId = serverInstance.Id,
+                            ConsumerStatusId = Convert.ToInt32(clientStatus),
+                            Created = DateTime.Now,
+                            Updated = DateTime.Now
+
+                        });
+                    }
+                    else
+                    {
+                        clientInstance.ConsumerStatusId = Convert.ToInt32(clientStatus);
+                        clientInstance.Updated = DateTime.Now;
+                        context.CloudClientInstance.Update(clientInstance);
+                       
+                    }
+                    context.SaveChanges();
+                    scope.Complete();
+                }
+            }
+        }
+
+        private CloudInstance GetCloudInstance(string threadGroup)
+        {
+            using(var context = new InteractContext())
+            {
+                return context.CloudInstance.Where(c => c.Threadgroup == threadGroup).FirstOrDefault();
             }
         }
     }
---
+++ Interact/Interact.Instance.Data/Exceptions/NotFoundException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Interact.Instance.Data.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+
+        public NotFoundException()
+          : base()
+        {
+        }
+
+        public NotFoundException(String message)
+          : base(message)
+        {
+        }
+
+        public NotFoundException(String message, Exception innerException)
+          : base(message, innerException)
+        {
+        }
+
+    }
+}
