682acf478d2deb017387bc7ba6d173936b4d20b8
NOTEDARTSMAR04\joao.costa <joao.costa@dartdigital.com.br>
2018-05-25 08:49:35 -0300

    Revert "Configuração do log4net"
    This reverts commit a5a46c52c25c5117eb571c87e6de625f14074035.

--- Interact/Interact.Instance/Program.cs
+++ Interact/Interact.Instance/Program.cs
@@ -1,37 +1,24 @@
-﻿using log4net.Config;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
 using System.Text;
-using System.Xml;
 
 namespace Interact.Instance
 {
     class Program
     {
-        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
-
         public static void Main(string[] args)
         {
-            ConfigureLog4Net();
+
         }
 
-        private static void ConfigureLog4Net()
-        {
-            XmlDocument log4netConfig = new XmlDocument();
-
-            log4netConfig.Load(File.OpenRead("log4net.config"));
-
-            var repo = log4net.LogManager.CreateRepository(Assembly.GetEntryAssembly(), typeof(log4net.Repository.Hierarchy.Hierarchy));
-
-            XmlConfigurator.Configure(repo, log4netConfig["log4net"]);
-        }
+        
 
         private static void LoadInstanceServices()
         {
             var services = new ServiceCollection();
+
+
         }
     }
 }
